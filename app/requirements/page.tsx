fa7f405bb84c55dd99f3c29381d4e19f7bfacd8c
StreamSurG <alberthollandg@gmail.com>
2026-07-16 11:07:01 -0700

    Wrap RequirementForm in Suspense

--- app/requirements/page.tsx
+++ app/requirements/page.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from "react";
+
 import RequirementHeader from "../../components/RequirementHeader";
 import RequirementForm from "../../components/RequirementForm";
 import RequirementSuccess from "../../components/RequirementSuccess";
@@ -10,12 +12,18 @@ export default function RequirementsPage() {
 
         <RequirementHeader />
 
-        <RequirementForm />
+        <Suspense
+          fallback={
+            <div className="mt-12 rounded-3xl border border-white/10 bg-[#101010] p-10 text-center text-white">
+              Loading requirements...
+            </div>
+          }
+        >
+          <RequirementForm />
+        </Suspense>
 
         <div className="mt-16">
-
           <RequirementSuccess />
-
         </div>
 
       </div>
